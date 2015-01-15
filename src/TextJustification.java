0260de96d704799a9ff2055a7f4930133d9bc860
Yijun Mao <siren0413@gmail.com>
2015-01-14 23:56:33 -0800

    update

--- src/TextJustification.java
+++ src/TextJustification.java
@@ -5,14 +5,8 @@ public class TextJustification {
 	public static List<String> justify(String[] words, int L){
 		if(words == null || words.length == 0) return new LinkedList<>();
 		int[] dp = new int[words.length];
-		List<String> list = new ArrayList<>();
 		minCost(words, 0, L, dp);
-		int i = 0, j = 0;
-		StringBuilder sb = new StringBuilder();
-		while(i < words.length){
-			
-		}
-		return list;
+		return null;
 	}
 	
 	private static int minCost(String[] words, int i, int L, int[] dp){
@@ -29,6 +23,8 @@ public class TextJustification {
 			if(L -len -(k-i) < 0) break;
 			if(k != words.length-1){
 				cost = (int)Math.pow((L - len - (k-i)),2) + minCost(words, k+1, L,  dp);
+			}else{
+				cost = 0;
 			}
 			if(cost < min){
 				min = cost;
@@ -41,6 +37,6 @@ public class TextJustification {
 	
 	public static void main(String[] args) {
 		String[] words = {"hello", "world","nihao"};
-		System.out.println(justify(words, 10));
+		System.out.println(justify(words, 13));
 	}
 }
